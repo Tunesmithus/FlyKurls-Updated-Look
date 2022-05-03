a7ac922413e8085a2474d3d47504d7af769c65a2
Channing Robertson <channing.robertson@gmail.com>
2022-05-03 06:22:42 -0500

    Add project files.

---
+++ FlyKurls.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FlyKurls.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlyKurls.Web.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> categories{ get; set; }
+    }
+}
